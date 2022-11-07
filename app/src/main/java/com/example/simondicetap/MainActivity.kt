2c322d0b87d0b554fd8bfbb73dbbe5915b32530a
MeloAguilar <carmelo.aguilar@iesnervion.es>
2022-11-07 11:17:26 +0100

    pipo

--- app/src/main/java/com/example/simondicetap/MainActivity.kt
+++ app/src/main/java/com/example/simondicetap/MainActivity.kt
@@ -2,140 +2,73 @@ package com.example.simondicetap
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
 
-    var velocidad : Velocidades? = null
-    var secuenciaCpu : MutableList<Colores> = mutableListOf()
-    var secuenciaUser : MutableList<Colores> = mutableListOf()
+    var velocidad: Velocidades? = null
+    var secuenciaCpu: MutableList<Colores> = mutableListOf()
+    var secuenciaUser: MutableList<Colores> = mutableListOf()
 
-    var pausa : Boolean = true
-
-
-
+    var pausa: Boolean = true
+    var colorApagado: Int = 0
+    var colorEncendido: Int = 0
+    var btnId: Int = 0
+    final var colorAzulEncendido: Int = R.drawable.btn_azul_encendido
+    final var colorVerdeEncendido: Int = R.drawable.btn_verde_encendido
+    final var colorAmarilloEncendido: Int = R.drawable.btn_amarillo_encendido
+    final var colorRojoEncendido: Int = R.drawable.btn_rojo_encendido
+    final var colorAzulApagado: Int = R.drawable.btn_azul_apagado
+    final var colorVerdeApagado: Int = R.drawable.btn_verde_apagado
+    final var colorRojoApagado: Int = R.drawable.btn_rojo_apagado
+    final var colorAmarilloApagado: Int = R.drawable.btn_amarillo_apagado
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*
+
         var btnNormal = findViewById<Button>(R.id.btnNormal)
         btnNormal.setOnClickListener { onClickBotonesInicio(1) }
         var btnLento = findViewById<Button>(R.id.btnLento)
         btnLento.setOnClickListener { onClickBotonesInicio(2) }
         var btnRapido = findViewById<Button>(R.id.btnRapido)
-        btnNormal.setOnClickListener { onClickBotonesInicio(3) }
+        btnRapido.setOnClickListener { onClickBotonesInicio(3) }
         var btnAuto = findViewById<Button>(R.id.btnAuto)
-        btnLento.setOnClickListener { onClickBotonesInicio(0) }
+        btnAuto.setOnClickListener { onClickBotonesInicio(0) }
         var btnAzul = findViewById<ImageView>(R.id.btnAzul)
         btnAzul.setOnClickListener { onClickBotonesColores(Colores.AZUL) }
         var btnAmarillo = findViewById<ImageView>(R.id.btnAmarillo)
-        btnAzul.setOnClickListener { onClickBotonesColores(Colores.AMARILLO) }
+        btnAmarillo.setOnClickListener { onClickBotonesColores(Colores.AMARILLO) }
         var btnVerde = findViewById<ImageView>(R.id.btnVerde)
-        btnAzul.setOnClickListener { onClickBotonesColores(Colores.VERDE) }
+        btnVerde.setOnClickListener { onClickBotonesColores(Colores.VERDE) }
         var btnRojo = findViewById<ImageView>(R.id.btnRojo)
-
- */
-        var btnAzul = findViewById<ImageView>(R.id.btnAzul)
-        btnAzul.setOnClickListener { //onClickBotonesColores(Colores.ROJO)
-            secuenciaParaProbar()}
-
-
-
-    }
-
-
-
-    fun secuenciaParaProbar(){
-        secuenciaCpu.add(Colores.ROJO)
-        secuenciaCpu.add(Colores.VERDE)
-        secuenciaCpu.add(Colores.ROJO)
-        secuenciaCpu.add(Colores.ROJO)
-        secuenciaCpu.add(Colores.AMARILLO)
-        secuenciaCpu.add(Colores.AZUL)
-        secuenciaCpu.add(Colores.VERDE)
-        var btn : ImageView? = null
-        for (item in secuenciaCpu){
-            var colorApagado :  Int = 0
-            var colorEncendido : Int = 0
-            var btnRes : Int
-
-            if(btn!=null){
-                btn.setImageResource(colorApagado)
-            }
-            when(item){
-                Colores.ROJO -> {
-                    colorApagado = R.drawable.btn_rojo_apagado
-                    colorEncendido = R.drawable.btn_rojo_encendido
-                    btnRes = R.id.btnRojo
-                }
-                Colores.VERDE -> {
-                    colorApagado = R.drawable.btn_verde_apagado
-                    colorEncendido = R.drawable.btn_verde_encendido
-                    btnRes = R.id.btnVerde
-                }
-                Colores.AMARILLO -> {
-                    colorApagado = R.drawable.btn_amarillo_apagado
-                    colorEncendido = R.drawable.btn_amarillo_encendido
-                    btnRes = R.id.btnAmarillo
-                }
-                Colores.AZUL -> {
-                    colorApagado = R.drawable.btn_azul_apagado
-                    colorEncendido = R.drawable.btn_azul_encendido
-                    btnRes = R.id.btnAzul
-                }
-            }
-
-            btn?.setImageResource(colorEncendido)
-
-        }
-    }
-
-
-
-    fun onClickBotonesColores(color:Colores){
-        secuenciaUser.add(color)
-        var colorApagadoRes : Int
-        var colorEncendidoRes : Int
-        var btnId : Int
-        if(secuenciaUser.size > 1){
-            when(color) {
-                Colores.ROJO ->{
-                    colorApagadoRes = R.drawable.btn_rojo_apagado
-                    btnId = R.id.btnRojo
-                    colorEncendidoRes = R.drawable.btn_rojo_encendido
-                }
-                Colores.AMARILLO ->{
-                    colorApagadoRes = R.drawable.btn_amarillo_apagado
-                    btnId = R.id.btnAmarillo
-                    colorEncendidoRes = R.drawable.btn_amarillo_encendido
-                }
-                Colores.VERDE ->{
-                    colorApagadoRes = R.drawable.btn_verde_apagado
-                    btnId = R.id.btnVerde
-                    colorEncendidoRes = R.drawable.btn_verde_encendido
-                }
-                Colores.AZUL ->{
-                    colorApagadoRes = R.drawable.btn_azul_apagado
-                    btnId = R.id.btnAzul
-                    colorEncendidoRes = R.drawable.btn_azul_encendido
-                }
-            }
-            var btn = findViewById<ImageView>(btnId)
-            btn.setImageResource(colorEncendidoRes)
-            TimeUnit.MILLISECONDS.sleep(100)
-            btn.setImageResource(colorApagadoRes)
+        btnRojo.setOnClickListener {
+            onClickBotonesColores(Colores.ROJO)
         }
 
+        quitarClickBotonesColores(btnRojo, btnAzul, btnVerde, btnAmarillo)
+
+
+
     }
 
-    fun onClickBotonesInicio(numBtn : Int) {
-        if (pausa) {
+    fun quitarClickBotonesColores(btn: View, btn2 : View, btn3:View, btn4:View){
+        btn.isClickable = false
+        btn2.isClickable = false
+        btn3.isClickable = false
+        btn4.isClickable = false
+    }
+
+    fun onClickBotonesInicio(numBtn: Int) {
+
             when (numBtn) {
                 0 -> {
                     var rnd = Random.nextInt(Velocidades.values().size)
@@ -156,54 +89,127 @@ class MainActivity : AppCompatActivity() {
             dialogo.setMessage("Mira")
             dialogo.setPositiveButton("Ok") { _, _ ->
                 pausa = false
-
+                generarPatronCPU()
 
             }
-            var ad : AlertDialog = dialogo.create() //show() MUST be called before dialog.getButton
+            findViewById<ImageView>(R.id.btnAmarillo).isClickable = true
+            findViewById<ImageView>(R.id.btnRojo).isClickable = true
+            findViewById<ImageView>(R.id.btnVerde).isClickable = true
+            findViewById<ImageView>(R.id.btnAzul).isClickable =true
+            var ad: AlertDialog =
+                dialogo.create()
             ad.show()
 
             ad.setCancelable(false)
-            generarPatronCPU()
-
-        }
-
-        else{
-
-        }
     }
 
 
-    fun generarPatronCPU(){
-        var rnd = Random.nextInt(Velocidades.values().size)
-        var color = Colores.values()[rnd]
-        var btnRojo = findViewById<ImageView>(R.id.btnRojo)
-        var btnAzul = findViewById<ImageView>(R.id.btnAzul)
-        var btnAmarillo = findViewById<ImageView>(R.id.btnAmarillo)
-        var btnVerde = findViewById<ImageView>(R.id.btnVerde)
-        var vel :Velocidades = velocidad as Velocidades
-        secuenciaCpu.add(color)
-        when(color){
+
+
+
+    fun onClickBotonesColores(color: Colores) {
+        secuenciaUser.add(color)
+        when (color) {
             Colores.ROJO -> {
-                btnRojo.performContextClick()
-            }
-            Colores.AZUL ->{
-                btnAzul.setImageResource(R.drawable.btn_azul_encendido)
-                TimeUnit.MILLISECONDS.sleep(vel.milis)
-                btnAzul.setImageResource(R.drawable.btn_azul_apagado)
-            }
-            Colores.VERDE -> {
-                btnVerde.setImageResource(R.drawable.btn_verde_encendido)
-                TimeUnit.MILLISECONDS.sleep(vel.milis)
-                btnVerde.setImageResource(R.drawable.btn_verde_apagado)
+                colorApagado = colorRojoApagado
+                btnId = R.id.btnRojo
+                colorEncendido = colorRojoEncendido
             }
             Colores.AMARILLO -> {
-                btnAmarillo.setImageResource(R.drawable.btn_amarillo_encendido)
-                TimeUnit.MILLISECONDS.sleep(vel.milis)
-                btnAmarillo.setImageResource(R.drawable.btn_amarillo_apagado)
+                colorApagado = colorAmarilloApagado
+                btnId = R.id.btnAmarillo
+                colorEncendido = colorAmarilloEncendido
+            }
+            Colores.VERDE -> {
+                colorApagado = colorVerdeApagado
+                btnId = R.id.btnVerde
+                colorEncendido = colorVerdeEncendido
+            }
+            Colores.AZUL -> {
+                colorApagado = colorAzulApagado
+                btnId = R.id.btnAzul
+                colorEncendido = colorAzulEncendido
             }
         }
 
+        var btn = findViewById<ImageView>(btnId)
+        btn.setImageResource(colorEncendido)
+
+        GlobalScope.launch {
+            delay(200)
+            btn?.setImageResource(colorApagado)
+        }
+    }
+
+
+fun comprobarClickUser(color : Colores, ronda : Int){
+    if(color == secuenciaCpu[ronda]){
+        generarPatronCPU()
+    }else{
+        findViewById<ImageView>(R.id.btnRojo).isClickable = false
+        findViewById<ImageView>(R.id.btnAmarillo).isClickable = false
+        findViewById<ImageView>(R.id.btnVerde).isClickable = false
+        findViewById<ImageView>(R.id.btnAzul).isClickable = false
+    }
+}
+
+
+    fun mostrarPatronCPU(){
+        for (item in secuenciaCpu){
+            for (itemUser in secuenciaUser){
+                if(item != (itemUser)){
+                    var dialogo = AlertDialog.Builder(this)
+                    dialogo.setMessage("Has Perdido")
+                    dialogo.setPositiveButton("Ok") { _, _ ->
+                        pausa = false
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun generarPatronCPU() {
+        var rnd = Random.nextInt(Velocidades.values().size)
+        var color = Colores.values()[rnd]
+        var btn: ImageView? = null
+        var vel: Velocidades = velocidad as Velocidades
+        secuenciaCpu.add(color)
+        when (color) {
+            Colores.ROJO -> {
+                btnId = R.id.btnRojo
+                btn = findViewById(btnId)
+                btn?.setImageResource(colorRojoEncendido)
+                colorApagado = colorRojoApagado
+
+            }
+            Colores.AZUL -> {
+                btnId = R.id.btnAzul
+                btn = findViewById(btnId)
+                btn?.setImageResource(colorAzulEncendido)
+                colorApagado = colorAzulApagado
+            }
+            Colores.VERDE -> {
+                btnId = R.id.btnVerde
+                btn = findViewById(btnId)
+                btn?.setImageResource(colorVerdeEncendido)
+                colorApagado = colorVerdeApagado
+            }
+            Colores.AMARILLO -> {
+                btnId = R.id.btnAmarillo
+                btn = findViewById(btnId)
+                btn?.setImageResource(colorAmarilloEncendido)
+                colorApagado = colorAmarilloApagado
+            }
+
+        }
+        GlobalScope.launch {
+            delay(100)
+            btn?.setImageResource(colorApagado)
+        }
 
 
     }
+
 }
